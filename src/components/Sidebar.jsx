37507f9a5dca3b29efd325f38d8b333da90e1cb0
suprayogo <126061696+suprayogo@users.noreply.github.com>
2023-10-20 20:10:09 +0700

    Upload Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../assets/styles/Sidebar.scss";
+import logoImage from "../assets/images/gallery.svg";
+import profileImage from "../assets/images/Frame.svg";
+import dashboardIcon from "../assets/images/category-2.svg";
+import { NavLink } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <>
+      <main id="sidebar-page">
+        <div className="body-background">
+          <div className="header">
+            <div className="sidebar">
+              <div className="d-flex align-items-center">
+                <div className=" box-gray mb-3">
+                  <img src={logoImage} alt="logo" />
+                </div>
+              </div>
+
+              <div className="user-profile d-flex">
+                <img src={profileImage} alt="Profile" />
+
+                <div className="name-profile ms-2">
+                  <h6>Lekan Okeowo</h6>
+                  <p>demo@gmail.com</p>
+                </div>
+              </div>
+
+              <div className="mt-4 link-active">
+                <ul>
+                  <NavLink
+                    to="/"
+                    className="text-decoration-none d-flex align-items-center"
+                  >
+                    <img
+                      src={dashboardIcon}
+                      alt="Dashboard Icon"
+                      className="icon"
+                    />
+                    Dashboard
+                  </NavLink>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Sidebar;
---
+++ src/page/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+
+function Home() {
+  return <>
+  <Sidebar/>
+
+  
+  
+  </>;
+}
+
+export default Home;
